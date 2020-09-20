8b38659f8bdb1c8107ade71d2457206b3d5519c8
jarrellrn <jarrellrn@gmail.com>
2020-09-20 11:06:37 -0500

    added bot login func

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,6 @@
 require("dotenv").config();
 
-console.log(process.env.DISCORDJS_BOT_TOKEN); // logs token
+const { Client, Message } = require("discord.js");
+const client = new Client();
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
