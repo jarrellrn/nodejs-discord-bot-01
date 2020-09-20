fe52032008d698e77c84461c560e15cd060d197a
jarrellrn <jarrellrn@gmail.com>
2020-09-20 11:32:38 -0500

    added reply and message functionality

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,21 @@
 require("dotenv").config();
 
-const { Client, Message } = require("discord.js");
+const { Client, Message, DiscordAPIError } = require("discord.js");
 const client = new Client();
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on("message", (message) => {
+  console.log(`[${message.author.tag}]: "${message.content}"`);
+  if (message.content === "test") {
+    //tags user
+    message.reply("test received");
+
+    //doesn't tag user
+    message.channel.send("test received");
+  }
+});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
